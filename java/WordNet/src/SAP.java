5e480775b327f5251b63392457c5f1dc469ae8f7
chu zui <chuqi77@gmail.com>
2013-03-27 13:33:32 +0800

    cc

---
+++ java/WordNet/src/SAP.java
@@ -0,0 +1,29 @@
+
+public class SAP {
+	public SAP(Digraph G)
+	{
+		
+	}
+	
+	public int length(int v, int w)
+	{
+		
+	}
+	
+	public int ancestor(int v, int w)
+	{
+		
+		
+	}
+	
+	public int length(Iterable<Integer> v, Iterable<Integer> w)
+	{
+		
+	}
+	
+	public int ancestor(Iterable<Integer> v, Iterable<Integer> w)
+	{
+		
+		
+	}
+}
---
+++ java/WordNet/src/WordNet.java
@@ -0,0 +1,33 @@
+
+public class WordNet {
+	
+	public WordNet(String synsets, String hypernyms)
+	{
+		
+	}
+	
+	public Iterable<String> nouns()
+	{
+		return null;
+	}
+	
+	public boolean isNoun(String word)
+	{
+		return false;
+	}
+	
+	public int distance(String nounA, String nounB)
+	{
+		return 0;
+	}
+	
+	public String sap(String nounA, String nounB)
+	{
+		return null;
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
+}
