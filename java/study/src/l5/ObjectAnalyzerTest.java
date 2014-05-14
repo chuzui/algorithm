8378460872b1c7d806f9a8b434f438357e331110
chu <zui>
2014-05-14 17:53:12 +0800

    add ObjectAnalyzerTest

---
+++ java/study/src/l5/ObjectAnalyzerTest.java
@@ -0,0 +1,16 @@
+package l5;
+
+import l4.Employee;
+
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2014/5/14.
+ */
+public class ObjectAnalyzerTest {
+    public static void main(String[] args)
+    {
+        Manager m = new Manager("hary", 10000, new Date(), 8000);
+        System.out.print(new ObjectAnalyzer().toString(m));
+    }
+}
