7053d15a257dca9d0d357ba6774aa1163a7641fb
chu <zui>
2014-05-15 00:05:18 +0800

    TimerTest

--- java/study/src/l5/TestGit.java
+++ java/study/src/l5/TestGit.java
@@ -1,7 +1,14 @@
 package l5;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * Created by Administrator on 2014/5/14.
  */
 public class TestGit {
+    public void test()
+    {
+
+    }
 }
---
+++ java/study/src/l6/TimerTest.java
@@ -0,0 +1,33 @@
+package l6;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
+
+/**
+ * Created by Administrator on 2014/5/14.
+ */
+public class TimerTest {
+    public static void main(String[] args)
+    {
+        ActionListener listener = new TimePrinter();
+
+        Timer t = new Timer(10000, listener);
+        t.start();
+        JOptionPane.showMessageDialog(null, "Quit program?");
+        System.exit(0);
+    }
+}
+
+class TimePrinter implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Date now = new Date();
+        System.out.println("the time is " + now);
+        Toolkit.getDefaultToolkit().beep();
+    }
+}
