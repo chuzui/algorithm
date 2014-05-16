4294390f4fceef32de5aa2ce47138c844e7710f5
chu <zui>
2014-05-16 09:08:05 +0800

    ProxyTest

---
+++ java/study/src/l11/StackTest.java
@@ -0,0 +1,30 @@
+package l11;
+import java.util.Scanner;
+
+import static java.lang.System.out;
+/**
+ * Created by Administrator on 2014/5/16.
+ */
+public class StackTest {
+    public static int factorial(int n)
+    {
+        System.out.println("factorial(" + n + ");");
+        Throwable t = new Throwable();
+        StackTraceElement[] frames = t.getStackTrace();
+        for (StackTraceElement f : frames)
+            System.out.println(f);
+        int r;
+        if (n < 1) r = 1;
+        else r = n * factorial(n - 1);
+        System.out.println("return " + r);
+        return r;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter n:");
+        int n = in.nextInt();
+        factorial(n);
+    }
+}
