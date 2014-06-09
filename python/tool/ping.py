b30e66146cc837df410334049d8c5775b1d319ec
chu <zui>
2014-06-10 00:20:25 +0800

    asd

---
+++ python/tool/ping.py
@@ -0,0 +1,31 @@
+__author__ = 'Administrator'
+
+import re
+import subprocess
+addresses = ['sg1.vpnreal.com',
+             'sg2.vpnreal.com',
+             'us1.vpnreal.com',
+             'us2.vpnreal.com',
+             'us3.vpnreal.com',
+             'us4.vpnreal.com',
+             'us5.vpnreal.com']
+
+avgRe = u'(?<=平均 = )[0-9]+'
+avgPattern = re.compile(avgRe)
+avgSpeedList = []
+shortestTime = 100000
+for address in addresses:
+
+    tmp = subprocess.Popen('ping ' + address, stdout=subprocess.PIPE).stdout.readlines()
+    tmpStr = ''.join((x.decode('gb2312') for x in tmp))
+
+    print(tmpStr)
+    m = avgPattern.search(tmpStr)
+    speed = int(m.group())
+    if shortestTime > speed:
+        shortestTime = speed
+        fastestAddress = address
+
+print('the fastest address is ' + fastestAddress)
+print('the average time is ' + str(shortestTime) + 'ms\n')
+input()
