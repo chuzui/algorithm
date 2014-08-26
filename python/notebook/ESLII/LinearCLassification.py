12e07ac8ebd84e718ad9aca6c418c27b140f5389
chuzui <chuqi77@gmail.com>
2014-08-26 20:38:25 +0800

    c

--- python/notebook/ESLII/LinearCLassification.py
+++ python/notebook/ESLII/LinearCLassification.py
@@ -1,6 +1,8 @@
+from __future__ import division
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+
 def loadVowelData(path):
     with open(path) as f:
         f.readline()
@@ -16,9 +18,28 @@ def dup_fig(x, y):
         inds = sp.nonzero(y == i)
         plt.plot(x[inds, 0], x[inds, 1], 'o', color=colors[i])
     plt.show()
+
+def reduced_rank_LDA(XTrain, yTrain, XTest, yTest):
+    K = len(sp.unique(yTrain))
+    N = XTrain.shape[0]
+    p = XTrain.shape[1]
+
+    PiK = sp.zeros((K, 1))
+    M = sp.zeros((K, p))
+    ScatterMatrix = []
+    for ci in range(1, K+1):
+        inds = sp.nonzero(yTrain == ci)
+        Nci = len(inds[0])
+        PiK[ci-1] = Nci / N
+        #print XTrain[inds, :]
+        M[ci-1, :] = sp.mean(XTrain[inds[0], :], 0)
+    print PiK
+    print M
+
+
 path = '../data/vowel.train'
 x, y = loadVowelData(path)
-dup_fig(x, y)
+reduced_rank_LDA(x,y, None, None)
 
 
 
