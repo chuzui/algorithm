fb44817cb683765a57bedd5d41d87e0efaa808d1
chuzui <chuqi77@gmail.com>
2014-09-01 13:25:06 +0800

    ss

--- python/notebook/ESLII/LinearCLassification.py
+++ python/notebook/ESLII/LinearCLassification.py
@@ -2,6 +2,22 @@ from __future__ import division
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+import numpy.linalg as ln
+def loadSAData(path):
+    data = {}
+    with open(path) as f:
+        data['name'] = f.readline().split(',')[1:-1]
+
+        XTrain = []
+        y = []
+        for l in f:
+            ls = l.split(',')
+            XTrain.append(map(float, ls[1:-1]))
+            y.append([float(ls[-1])])
+        data['XTrain'] = np.array(XTrain)
+        data['y'] = np.array(y)
+        return data
+
 
 def loadVowelData(path):
     with open(path) as f:
@@ -36,10 +52,61 @@ def reduced_rank_LDA(XTrain, yTrain, XTest, yTest):
     print PiK
     print M
 
+def processData(X, **opts):
+    N,M = X.shape
+    if opts.get('std', True):
+        X = X - np.mean(X, 0)
+    if opts.get('scale', True):
+        X = X / (X.max(0) - X.min(0))
+    if opts.get('addones', True):
+        X = sp.c_[sp.ones((N,1)), X]
+    return X
 
-path = '../data/vowel.train'
-x, y = loadVowelData(path)
-reduced_rank_LDA(x,y, None, None)
+def sigmoid(inX):
+    return 1.0 / (1 + sp.exp(-inX))
+
+def logRegres(XTrain, y, **opts):
+    CONV_THRESH = 1.e-15
+    N, P = XTrain.shape
+    maxIter = opts.get('maxIter', 100000)
+    beta = sp.zeros((P, 1))
+    XT = XTrain.T
+    for i in xrange(maxIter):
+        betaOld = beta
+        ebx = np.exp(np.dot(XTrain, beta))
+        p = ebx / (1.+ebx)
+        s = np.dot(XT, y-p)
+        JBar = np.dot(XT, XTrain * np.multiply(p, 1.-p))
+        beta = betaOld + np.dot(ln.inv(JBar), s)
+        diff = np.sum(np.fabs(beta - betaOld))
+        if diff < CONV_THRESH:
+            break
+    return beta
+
+def SATest():
+    data = loadSAData('../data/SAheart.data')
+    XTrain = processData(data['XTrain'], std=False, scale=False)
+    XTrain = XTrain[:, :][:, [0,1,2,3,5,7,8,9]]
+    N, P = XTrain.shape
+    y = data['y']
+    name = ['intercept']
+    name.append(data['name'])
+    beta = logRegres(XTrain, y, maxIter=100000)
+    m = ln.inv(XTrain.T.dot(XTrain))
+    ebx = np.exp(np.dot(XTrain, beta))
+    p = ebx / (1.+ebx)
+    print p
+    sigma = np.sum((y - p) ** 2) / (N - P - 1)
+    print sigma
+    covarBetaHat  =  m
+    print covarBetaHat.shape
+    for i in range(covarBetaHat.shape[0]):
+        print sp.sqrt(covarBetaHat[i][i])
+SATest()
+
+# path = '../data/vowel.train'
+# x, y = loadVowelData(path)
+# reduced_rank_LDA(x,y, None, None)
 
 
 
